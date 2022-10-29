cd4746e554c88aa0521742769bb8000b8f6dcf65
cyberjam <jamsince1994@naver.com>
2022-10-29 21:45:06 +0900

    실종날짜별 폴더 저장

--- utils/image_pipeline.py
+++ utils/image_pipeline.py
@@ -8,14 +8,15 @@ def save_images(IMG_PATH, previous_images, current_images, df):
 
     for item_dict in tqdm(current_images_json):
         responds = requests.get(item_dict['popfile'])
-        open(f"{IMG_PATH}/{item_dict['desertionNo']}_{item_dict['happenDt']}.jpg", "wb").write(responds.content)
+        os.makedirs(f"{IMG_PATH}/{item_dict['happenDt']}", exist_ok=True)
+        open(f"{IMG_PATH}/{item_dict['happenDt']}/{item_dict['desertionNo']}.jpg", "wb").write(responds.content)
     # return
 
 def remove_images(IMG_PATH, previous_images, current_images, df):
     delete_images = previous_images - current_images
     delete_images_json = df[df["desertionNo"].isin(delete_images)][["desertionNo","happenDt"]].to_dict("records")
     for item_dict in tqdm(delete_images_json):
-        os.makedirs(f"{IMG_PATH}/{item_dict['happenDt']}", exist_ok=True)
+        
         os.remove(f"{IMG_PATH}/{item_dict['happenDt']}/{item_dict['desertionNo']}.jpg")
 
 def image_pipeline(df, IMG_PATH):
