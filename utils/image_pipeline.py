1d0d2e6c6878657ae1989f45b23c09de8b7a81a2
cyberjam <jamsince1994@naver.com>
2022-10-31 23:07:01 +0900

    이미지 삭제 시 DB에서 happendDt 조회

--- utils/image_pipeline.py
+++ utils/image_pipeline.py
@@ -2,9 +2,9 @@ import os
 import requests
 from tqdm import tqdm
 
-def save_images(IMG_PATH, previous_images, current_images, df):
+def save_images(IMG_PATH, previous_images, current_images, api_df):
     create_images = current_images - previous_images
-    current_images_json = df[df["desertionNo"].isin(create_images)][["desertionNo","popfile","happenDt"]].to_dict("records")
+    current_images_json = api_df[api_df["desertionNo"].isin(create_images)][["desertionNo","popfile","happenDt"]].to_dict("records")
 
     for item_dict in tqdm(current_images_json):
         responds = requests.get(item_dict['popfile'])
@@ -12,17 +12,17 @@ def save_images(IMG_PATH, previous_images, current_images, df):
         open(f"{IMG_PATH}/{item_dict['happenDt']}/{item_dict['desertionNo']}.jpg", "wb").write(responds.content)
     # return
 
-def remove_images(IMG_PATH, previous_images, current_images, df):
+def remove_images(IMG_PATH, previous_images, current_images, db_df):
     delete_images = previous_images - current_images
-    delete_images_json = df[df["desertionNo"].isin(delete_images)][["desertionNo","happenDt"]].to_dict("records")
+    delete_images_json = db_df[db_df["desertionNo"].isin(delete_images)][["desertionNo","happenDt"]].to_dict("records")
     for item_dict in tqdm(delete_images_json):
         
         os.remove(f"{IMG_PATH}/{item_dict['happenDt']}/{item_dict['desertionNo']}.jpg")
 
-def image_pipeline(df, IMG_PATH):
+def image_pipeline(api_df, db_df, IMG_PATH):
     os.makedirs(IMG_PATH, exist_ok=True)
     previous_images = set([os.path.splitext(file)[0] for _, _, files in os.walk(IMG_PATH) for file in files])
-    current_images = set(df["desertionNo"].to_list())
+    current_images = set(api_df["desertionNo"].to_list())
     
-    save_images(IMG_PATH, previous_images, current_images, df)
-    remove_images(IMG_PATH, previous_images, current_images, df)
+    save_images(IMG_PATH, previous_images, current_images, api_df)
+    remove_images(IMG_PATH, previous_images, current_images, db_df)
--- utils/querys.py
+++ utils/querys.py
@@ -6,4 +6,8 @@ def make_query_insert(fields):
 def make_query_truncate(table_name):
       query=f'''TRUNCATE TABLE {table_name};'''
       return query
+
+def make_query_select():
+      query = f'''SELECT * FROM animal_info'''
+      return query
       
